0e7f0fa068823722ff9f81858d63ecd3f88bb05f
Development Team <dev@digitalspider.com.au>
2014-08-24 02:05:56 +1000

    publish plugin script

---
+++ publish.sh
@@ -0,0 +1,5 @@
+cp ./MyPlugin/dist/MyPlugin-1.0-SNAPSHOT.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
+#cp ./TreePlugin/dist/TreePlugin-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
+cp ./GoComicPlugin/dist/GoComicPlugin-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
+cp ./ImageGalleryPlugin/dist/ImageGallery-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
+unzip -q -o ImageGalleryPlugin/dist/jssor.zip -d /opt/digitalspider/jspwiki/JSPWiki/
