99322c45f1ee0322906b4cd4082d697e336f2fba
Development Team <dev@digitalspider.com.au>
2014-09-03 21:00:35 +1000

    updated publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,16 @@
-cp ./MyPlugin/dist/MyPlugin-1.0-SNAPSHOT.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
-#cp ./TreePlugin/dist/TreePlugin-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
-cp ./GoComicPlugin/dist/GoComicPlugin-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
-cp ./ImageGalleryPlugin/dist/ImageGallery-0.1.jar /opt/digitalspider/jspwiki/JSPWiki/WEB-INF/lib/
-unzip -q -o ImageGalleryPlugin/dist/jssor.zip -d /opt/digitalspider/jspwiki/JSPWiki/
+#!/bin/bash
+
+DSDIR=/opt/digitalspider
+PLUGINSDIR=$DSDIR/jspwiki-plugins
+WIKIDIR=$CATALINA_HOME/webapps/JSPWiki
+WEBLIBDIR=$WIKIDIR/WEB-INF/lib
+
+cp $PLUGINSDIR/MyPlugin/dist/MyPlugin-0.1.jar $WEBLIBDIR/
+cp $PLUGINSDIR/TreePlugin/dist/TreePlugin-0.9.jar $WEBLIBDIR/
+cp $PLUGINSDIR/HitCountPlugin/dist/HitCountPlugin-0.1.jar $WEBLIBDIR/
+cp $PLUGINSDIR/GoComicsPlugin/dist/GoComicsPlugin-0.1.jar $WEBLIBDIR/
+cp $PLUGINSDIR/ImageGalleryPlugin/dist/ImageGalleryPlugin-0.1.jar $WEBLIBDIR/
+cp $PLUGINSDIR/VideoPlugin/dist/VideoPlugin-0.1.jar $WEBLIBDIR/
+cp $PLUGINSDIR/JiraPlugin/dist/JiraPlugin-0.1.jar $WEBLIBDIR/
+unzip -q -o $PLUGINSDIR/ImageGalleryPlugin/dist/jssor-templates.zip -d $WIKIDIR/
+
