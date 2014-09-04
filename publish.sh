5df8d345cf9d0b96b2c878850547d665c27878ac
Development Team <dev@digitalspider.com.au>
2014-09-04 23:58:30 +1000

    Added AndroidStudio iml files, updated publish.sh

--- publish.sh
+++ publish.sh
@@ -5,12 +5,13 @@ PLUGINSDIR=$DSDIR/jspwiki-plugins
 WIKIDIR=$CATALINA_HOME/webapps/JSPWiki
 WEBLIBDIR=$WIKIDIR/WEB-INF/lib
 
-cp $PLUGINSDIR/MyPlugin/dist/MyPlugin-0.1.jar $WEBLIBDIR/
-cp $PLUGINSDIR/TreePlugin/dist/TreePlugin-0.9.jar $WEBLIBDIR/
-cp $PLUGINSDIR/HitCountPlugin/dist/HitCountPlugin-0.1.jar $WEBLIBDIR/
-cp $PLUGINSDIR/GoComicsPlugin/dist/GoComicsPlugin-0.1.jar $WEBLIBDIR/
-cp $PLUGINSDIR/ImageGalleryPlugin/dist/ImageGalleryPlugin-0.1.jar $WEBLIBDIR/
-cp $PLUGINSDIR/VideoPlugin/dist/VideoPlugin-0.1.jar $WEBLIBDIR/
-cp $PLUGINSDIR/JiraPlugin/dist/JiraPlugin-0.1.jar $WEBLIBDIR/
-unzip -q -o $PLUGINSDIR/ImageGalleryPlugin/dist/jssor-templates.zip -d $WIKIDIR/
+cp $PLUGINSDIR/MyPlugin/dist/*.jar $WEBLIBDIR/
+cp $PLUGINSDIR/TreePlugin/dist/*.jar $WEBLIBDIR/
+cp $PLUGINSDIR/HitCountPlugin/dist/*.jar $WEBLIBDIR/
+cp $PLUGINSDIR/GoComicsPlugin/dist/*.jar $WEBLIBDIR/
+unzip -q -o $PLUGINSDIR/ImageGalleryPlugin/dist/*.zip -d $WIKIDIR/
+cp $PLUGINSDIR/ImageGalleryPlugin/dist/*.jar $WEBLIBDIR/
+cp $PLUGINSDIR/VideoPlugin/dist/*.jar $WEBLIBDIR/
+cp $PLUGINSDIR/JiraPlugin/dist/*.jar $WEBLIBDIR/
+unzip -q -o $PLUGINSDIR/JiraPlugin/dist/*.zip -d $WEBLIBDIR/
 
