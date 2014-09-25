cb499b62b440009b96bcbaf4a5215df08fb10879
Development Team <dev@digitalspider.com.au>
2014-09-26 00:41:51 +1000

    updated panel

--- PanelPlugin/etc/panel/panel.js
+++ PanelPlugin/etc/panel/panel.js
@@ -1,8 +1,8 @@
 var selectedid="";
 var selectedclass="";
 var selectmode=false;
-var selectattr="selectBackground";
-var selectalter="selectId";
+var selectattr="selectBackgroundColor";
+var changeElement="selectId";
 
 function select(itemid,classid) {
   selectedid=itemid;
@@ -10,54 +10,215 @@ function select(itemid,classid) {
   selectmode=true;
   openColorMap();
 }
-function selectAttribute(attributeid) {
-  selectattr=attributeid;
+function selectStyleColor(valueid) {
+  selectattr=valueid;
+  showId("colorInput");
+  hideId("styleInput");
+  hideId("customInput");
+  removeAllStyleMarks();
+  addClassId(valueid,"mark");
 }
-function selectAlter(alterid) {
-  selectalter=alterid;
+function selectStyle(valueid) {
+  selectattr=valueid;
+  if(selectattr=='selectCustom') {
+    hideId("colorInput");
+    hideId("styleInput");
+    showId("customInput");
+  } else {
+    hideId("colorInput");
+    showId("styleInput");
+    setValueId("styleInput",getStyleId(selectedid,valueid));
+    hideId("customInput");
+  }
+  removeAllStyleMarks();
+  addClassId(valueid,"mark");
 }
-function clickColor(hex) {
+function removeAllStyleMarks() {
+  removeClassId("selectTextColor","mark");
+  removeClassId("selectBackgroundColor","mark");
+  removeClassId("selectBorderColor","mark");
+  removeClassId("selectFont","mark");
+  removeClassId("selectFontSize","mark");
+  removeClassId("selectBorder","mark");
+  removeClassId("selectCorners","mark");
+  removeClassId("selectPadding","mark");
+  removeClassId("selectMargin","mark");
+  removeClassId("selectMinWidth","mark");
+  removeClassId("selectMinHeight","mark");
+  removeClassId("selectScroll","mark");
+  removeClassId("selectCustom","mark");
+}
+function selectChangeElement(changeElementId) {
+  changeElement=changeElementId;
+  showId("styleSelectorColor");
+  showId("styleSelector");
+  removeAllElementMarks();
+  addClassId(changeElementId,"mark");
+}
+function removeAllElementMarks() {
+  removeClassId("selectId","mark");
+  removeClassId("selectClass","mark");
+  removeClassId("selectBody","mark");
+}
+function alterColor(hex) {
   if (selectmode) {
-    if (selectalter == 'selectId') {
+    if (changeElement == 'selectId') {
       var item = document.getElementById(selectedid);
-      changeColor(item,hex);
-    } else if (selectalter == 'selectClass') {
+      changeColor(item,selectattr,hex);
+    } else if (changeElement == 'selectClass') {
       var panels = document.getElementsByClassName(selectedclass);
       for (i = 0; i < panels.length; i++) {
         var panel = panels[i];
-        changeColor(panel,hex);
+        changeColor(panel,selectattr,hex);
       }
+    } else if (changeElement == 'selectBody') {
+      var item = document.getElementsByTagName('body')[0];
+      changeColor(item,selectattr,hex);
     }
   }
 }
-function changeColor(ele,hex) {
-  if (selectattr == "selectBackground") {
+function changeColor(ele,selectAttId,hex) {
+  if (selectAttId == "selectBackgroundColor") {
     ele.style.backgroundColor=hex;
-  } else if (selectattr == "selectText") {
+  } else if (selectAttId == "selectTextColor") {
     ele.style.color=hex;
+  } else if (selectAttId == "selectBorderColor") {
+    ele.style.borderColor=hex;
   }
 }
+function alterStyle(value) {
+  if (selectmode) {
+    if (changeElement == 'selectId') {
+      var item = document.getElementById(selectedid);
+      changeStyle(item,selectattr,value);
+    } else if (changeElement == 'selectClass') {
+      var panels = document.getElementsByClassName(selectedclass);
+      for (i = 0; i < panels.length; i++) {
+        var panel = panels[i];
+        changeStyle(panel,selectattr,value);
+      }
+    } else if (changeElement == 'selectBody') {
+      var item = document.getElementsByTagName('body')[0];
+      changeStyle(item,selectattr,value);
+    }
+  }
+}
+function changeStyle(ele,selectAttId,value) {
+  if (selectAttId == "selectFont") {
+    ele.style.font=value;
+  } else if (selectAttId == "selectFontSize") {
+    ele.style.fontSize=value;
+  } else if (selectAttId == "selectBorder") {
+    ele.style.border=value;
+  } else if (selectAttId == "selectCorners") {
+    ele.style.borderRadius=value;
+  } else if (selectAttId == "selectPadding") {
+    ele.style.padding=value;
+  } else if (selectAttId == "selectMargin") {
+    ele.style.margin=value;
+  } else if (selectAttId == "selectMinWidth") {
+    ele.style.minWidth=value;
+  } else if (selectAttId == "selectMinHeight") {
+    ele.style.minHeight=value;
+  } else if (selectAttId == "selectScroll") {
+    ele.style.overflow=value;
+  } else if (selectAttId == "selectCustom") {
+    ele.style.custom=value;
+  }
+}
+function getStyleId(eleId,selectAttId) {
+  ele=document.getElementById(eleId);
+  return getStyle(ele,selectAttId);
+}
+function getStyle(ele,selectAttId) {
+  if (selectAttId == "selectFont") {
+    return ele.style.font;value;
+  } else if (selectAttId == "selectFontSize") {
+    return ele.style.fontSize;
+  } else if (selectAttId == "selectBorder") {
+    return ele.style.border;
+  } else if (selectAttId == "selectCorners") {
+    return ele.style.borderRadius;
+  } else if (selectAttId == "selectPadding") {
+    return ele.style.padding;
+  } else if (selectAttId == "selectMargin") {
+    return ele.style.margin;
+  } else if (selectAttId == "selectMinWidth") {
+    return ele.style.minWidth;
+  } else if (selectAttId == "selectMinHeight") {
+    return ele.style.minHeight;
+  } else if (selectAttId == "selectScroll") {
+    return ele.style.overflow;
+  } else if (selectAttId == "selectCustom") {
+    return ele.style.custom;
+  }
+}
+
 function openColorMap() {
-  document.getElementById("colorSelectDiv").style.display="block";
+  showId("colorSelectDiv");
 }
 function closeColorMap() {
-  document.getElementById("colorSelectDiv").style.display="none";
+  hideId("colorSelectDiv");
   selectattr="selectBackground";
-  selectalter="selectId";
+  changeElement="selectId";
+  removeAllElementMarks();
+  removeAllStyleMarks();
 }
 function toggleEditMode(itemid,classid) {
   var panelid = 'panel-'+itemid;
   var panel = document.getElementById(panelid);
+  var editEle = document.getElementById(itemid);
   if (hasClass(panel,'selected')) {
-    removeClass(panel,'selected');
     disableClick(itemid,classid);
+    editEle.innerHTML="Edit";
+    closeColorMap();
+    hideId("styleSelectorColor");
+    hideId("styleSelector");
+    hideId("colorInput");
+    hideId("styleInput");
+    hideId("customInput");
+    removeClass(panel,'selected');
   } else {
     addClass(panel,'selected');
+    select("content-"+itemid,"content-"+classid)
     enableClick(itemid,classid);
+    editEle.innerHTML="Close";
+    openColorMap();
   }
 }
-var clickFunction = function(itemid,classid) {
-  select(this.id,this.className.substring(7))
+function toggleDisplayId(eleId) {
+  ele=document.getElementById(eleId);
+  toggleDisplay(ele);
+}
+function toggleDisplay(ele) {
+  if (ele) {
+    if (ele.style.display=="block") {
+      hide(ele);
+    } else {
+      show(ele);
+    }
+  }
+}
+function showId(eleId) {
+  ele=document.getElementById(eleId);
+  show(ele);
+}
+function hideId(eleId) {
+  ele=document.getElementById(eleId);
+  hide(ele);
+}
+function show(ele) {
+  if (ele) {
+    ele.style.display="block";
+  }
+}
+function hide(ele) {
+  if (ele) {
+    ele.style.display="none";
+  }
+}
+var clickFunction = function() {
+  select(this.id,this.className.substring(7));
 }
 function enableClick(itemid,classid) {
   var panel = document.getElementById("panel-"+itemid);
@@ -79,14 +240,33 @@ function disableClick(itemid,classid) {
   footer.removeEventListener("click", clickFunction, true );
   panel.style.cursor=null;
 }
+function setValueId(eleId,value) {
+  var ele = document.getElementById(eleId);
+  return setValue(ele,value);
+}
+function setValue(ele,value) {
+  return ele.value = value;
+}
+function hasClassId(eleId,cls) {
+  var ele = document.getElementById(eleId);
+  return hasClass(ele,cls);
+}
 function hasClass(ele,cls) {
   return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
 }
 
+function addClassId(eleId,cls) {
+  var ele = document.getElementById(eleId);
+  addClass(ele,cls);
+}
 function addClass(ele,cls) {
   if (!hasClass(ele,cls)) ele.className += " "+cls;
 }
 
+function removeClassId(eleId,cls) {
+  var ele = document.getElementById(eleId);
+  removeClass(ele,cls);
+}
 function removeClass(ele,cls) {
   if (hasClass(ele,cls)) {
     var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
